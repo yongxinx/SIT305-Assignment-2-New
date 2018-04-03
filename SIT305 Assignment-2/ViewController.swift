63a33058fb7e03e5543b6ae6c1e56280e0690b2a
NICK XIE <yongxinx@10-151-189-167.dhcp-m.deakin.edu.au>
2018-04-03 23:30:23 +1000

    Initial Commit

---
+++ SIT305 Assignment-2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SIT305 Assignment-2
+//
+//  Created by NICK XIE on 3/4/18.
+//  Copyright © 2018 NICK XIE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
