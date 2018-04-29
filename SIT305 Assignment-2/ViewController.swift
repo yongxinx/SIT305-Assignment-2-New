ce5f0b81d8ad12e13a41fec00a3d02936f57847f
YONGXIN XIE <nickxieeeee@gmail.com>
2018-04-29 17:34:13 +1000

    updated

--- SIT305 Assignment-2/ViewController.swift
+++ SIT305 Assignment-2/ViewController.swift
@@ -10,15 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var testLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        if let path = Bundle.main.path(forResource: "item", ofType: "json") {
+            do {
+                let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
+                let jsonResult = try JSONSerialization.jsonObject(with: data, options: .mutableLeaves)
+                if let jsonResult = jsonResult as? Dictionary<String, AnyObject>, let item = jsonResult["item"] as? [Any] {
+                    print(item)
+                }
+            } catch {
+                // handle error
+            }
+        }
+        
+      
+//        let path = Bundle.main.path(forResource: "item", ofType: "json")
+//        let url = URL(fileURLWithPath: path! )
+//
+//        do{
+//            let data = try Data(contentsOf: url)
+//            let item = try JSONDecoder().decode([String : [Int]].self, from: data)
+//            print(item)
+//        }
+//        catch{
+//
+//        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
